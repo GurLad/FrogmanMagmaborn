1987d27ae7a246f91d6df5dd3605b00c8cfc1a39
Gur Lad <40135047+GurLad@users.noreply.github.com>
2023-04-23 14:39:47 +0300

    Working on endgame animations

--- Assets/Scripts/Misc/Endgame/EndgameMapAnimator.cs
+++ Assets/Scripts/Misc/Endgame/EndgameMapAnimator.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class EndgameMapAnimator : MonoBehaviour
 {
+    [Header("Base")]
+    public AdvancedSpriteSheetAnimation PseudoTile;
     [Header("Floor")]
     public string FloorName;
     public EndgameFloorAnimator FloorBaseHolder;
+    public Sprite FloorBottomL;
+    public Sprite FloorBottomM;
+    public Sprite FloorBottomR;
+    [Header("Bridge")]
+    public string BridgeName;
+    public GameObject BridgeBaseHolder;
+    public Sprite BridgeTransparent;
+    public Sprite BridgeTop;
+    public Sprite BridgeBottom;
 
     private void Start()
     {
@@ -17,8 +28,6 @@ public class EndgameMapAnimator : MonoBehaviour
     public void Process(Tile[,] map, Vector2Int size)
     {
         // Clone map
-        // Item1 - was this tile processed already?
-        // Item2 - the tile itself
         ProcessedTile[,] tiles = new ProcessedTile[size.x, size.y];
         for (int i = 0; i < size.x; i++)
         {
@@ -46,10 +55,17 @@ public class EndgameMapAnimator : MonoBehaviour
 
     private void GroupFloor(ProcessedTile[,] tiles, Vector2Int size, int x, int y)
     {
+        // Group
         EndgameFloorAnimator holder = Instantiate(FloorBaseHolder, FloorBaseHolder.transform.parent);
         int height = size.y;
+        int width = size.x;
         for (int i = x; i < size.x; i++)
         {
+            if (tiles[i, y].Tile.Name != FloorName)
+            {
+                width = i;
+                break;
+            }
             for (int j = y; j < height; j++)
             {
                 if (tiles[i, j].Tile.Name != FloorName)
@@ -59,14 +75,35 @@ public class EndgameMapAnimator : MonoBehaviour
                 }
                 tiles[i, j].Processed = true;
                 tiles[i, j].Tile.transform.parent = holder.transform;
+                tiles[i, j].Tile.transform.position -= new Vector3(0, 0, 0.2f);
             }
-            if (tiles[i, y].Tile.Name != FloorName)
-            {
-                break;
-            }
+        }
+        // Add bottom
+        for (int i = x; i < width; i++)
+        {
+            AdvancedSpriteSheetAnimation newTile = Instantiate(PseudoTile, holder.transform);
+            InitNewTile(newTile, i == x ? FloorBottomL : (i == width - 1 ? FloorBottomR : FloorBottomM));
+            newTile.transform.position = new Vector2(GameController.Current.TileSize * i, -GameController.Current.TileSize * height);
+            newTile.gameObject.SetActive(true);
         }
         holder.transform.position -= new Vector3(0, 0, 0.1f);
         holder.gameObject.SetActive(true);
+    }
+
+    private void GroupBridge(ProcessedTile[,] tiles, Vector2Int size, int x, int y)
+    {
+        // TBA
+    }
+
+    private void InitNewTile(AdvancedSpriteSheetAnimation newTile, Sprite targetSprite)
+    {
+        SpriteSheetData newData = new SpriteSheetData();
+        newData.SpriteSheet = targetSprite;
+        newData.NumberOfFrames = (int)newData.SpriteSheet.rect.width / 16;
+        newData.Speed = 0;
+        newData.Name = "FloorBottom";
+        newData.Loop = true;
+        newTile.Animations.Add(newData);
     }
 
     private class ProcessedTile
