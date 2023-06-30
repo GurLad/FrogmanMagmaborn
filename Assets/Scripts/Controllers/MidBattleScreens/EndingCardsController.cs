30049156a8486f5d6cf2c0ecd3b4c61e58946ba9
Gur Ladizhinsky <40135047+GurLad@users.noreply.github.com>
2023-06-30 19:55:17 +0300

    Working on ending title cards
    Need to add a matching Frog Forge editor

---
+++ Assets/Scripts/Controllers/MidBattleScreens/EndingCardsController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingCardsController : MidBattleScreen
+{
+    public List<CharacterEndingData> CharacterEndings;
+    public EndingCardHolder EndingCardHolder;
+    private int currentCharacter;
+
+    public void DisplayNext()
+    {
+        if (currentCharacter < CharacterEndings.Count)
+        {
+            EndingCardData current = null;
+            do
+            {
+                current = CharacterEndings[currentCharacter].EndingCards.Find(a => new ConversationData("~\n" + a.Requirements + "\n~\n~\n").MeetsRequirements());
+            } while (current == null && ++currentCharacter < CharacterEndings.Count);
+            if (current != null)
+            {
+                EndingCardHolder.Display(CharacterEndings[currentCharacter].CharacterName, current.Title, current.Card);
+                currentCharacter++;
+            }
+            else
+            {
+                DisplayNext();
+            }
+        }
+        else
+        {
+            // TBA
+        }
+    }
+
+    [System.Serializable]
+    public class CharacterEndingData
+    {
+        public string CharacterName;
+        public List<EndingCardData> EndingCards;
+    }
+
+    [System.Serializable]
+    public class EndingCardData
+    {
+        public string Requirements;
+        public string Title;
+        public string Card;
+    }
+}
---
+++ Assets/Scripts/Misc/GameUI/EndingCardHolder.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndingCardHolder : MonoBehaviour
+{
+    private enum State { Idle, Writing, Waiting }
+
+    [Header("Vars")]
+    public float Speed;
+    public float WaitTime;
+    [Header("Objects")]
+    public EndingCardsController EndingCardsController;
+    public PortraitHolder PortraitHolder;
+    public Text Title;
+    public Text Stats;
+    public Text Card;
+    private State state;
+    private string cardText = "";
+    private int currentLetter = 0;
+    private float count;
+
+    private void Update()
+    {
+        switch (state)
+        {
+            case State.Idle:
+                break;
+            case State.Writing:
+                count += Time.deltaTime * Speed;
+                if (count >= 1)
+                {
+                    count--;
+                    Card.text += cardText[currentLetter++];
+                    if (currentLetter >= cardText.Length)
+                    {
+                        cardText = "";
+                        currentLetter = 0;
+                        count = 0;
+                        state = State.Waiting;
+                    }
+                }
+                break;
+            case State.Waiting:
+                count += Time.deltaTime;
+                if (count >= WaitTime)
+                {
+                    count = 0;
+                    state = State.Idle;
+                    PaletteController.Current.FadeOut(() => EndingCardsController.DisplayNext());
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void Display(string characterName, string title, string card)
+    {
+        Title.text = title;
+        DisplayUnitStats(characterName);
+        PortraitHolder.Portrait = PortraitController.Current.FindPortrait(characterName);
+        GameController.Current.LevelMetadata.SetPalettesFromMetadata();
+        PaletteController.Current.FadeIn(() => cardText = card);
+    }
+
+    private void DisplayUnitStats(string unit)
+    {
+        Stats.text = "Maps: " + SavedData.Load("Statistics", unit + "MapsCount", 1).ToString().PadRight(3) +
+            "  Fights: " + SavedData.Load("Statistics", unit + "BattleCount", 1).ToString().PadRight(3) +
+            "\nWins: " + SavedData.Load("Statistics", unit + "KillCount", 1).ToString().PadRight(3) +
+            "  Losses: " + SavedData.Load("Statistics", unit + "DeathCount", 1).ToString().PadRight(3);
+    }
+}
